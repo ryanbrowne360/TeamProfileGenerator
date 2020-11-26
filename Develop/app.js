39efcc6839363a583615fbb22914f31581918168
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-26 18:44:30 -0500

    app and readme updated

--- Develop/app.js
+++ Develop/app.js
@@ -4,32 +4,125 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+var teamInfo = [];
+var idValue = 1;
 const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+async function teamData() {
+    const managerInfo = await inquirer.prompt([
+        {
+            type: "input",
+            name: "n",
+            message: "Please add the name of the manager."
+        },
+        {
+            type: "input",
+            name: "e",
+            message: "Please add the email of the manager."
+        },
+        {
+            type: "input",
+            name: "o",
+            message: "Please add the office number for the manager."
+        },
+        {
+            type: "input",
+            name: "tN",
+            message: "Please add the number of team members."
+        }]
+    )
+
+    const manager = new Manager(managerInfo.n, idValue++, managerInfo.e, managerInfo.o)
+    teamInfo.push(manager)
+
+    for (let i = 0; i < managerInfo.tN; i++) {
+        const employeeInfo = await inquirer.prompt([
+            {
+                type: "list",
+                name: "role",
+                message: `What is the role of employee ${i + 1}?`,
+                choices: ["Engineer", "Intern"]
+            }]
+        )
+
+        // HINT: each employee type (manager, engineer, or intern) has slightly different
+        // information; write your code to ask different questions via inquirer depending on
+        // employee type.
+
+        if (employeeInfo.role == "Engineer") {
+            const engineerInfo = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "n",
+                    message: "Please add the name of the engineer."
+                },
+                {
+                    type: "input",
+                    name: "e",
+                    message: "Please add the email of the emgineer."
+                },
+                {
+                    type: "input",
+                    name: "g",
+                    message: "Please add the github username for the engineer."
+                }]
+            )
+            const engineer = new Engineer (engineerInfo.n, idValue++, engineerInfo.e, engineerInfo.g)
+            teamInfo.push(engineer)
+        }
+        else {
+            const internInfo = await inquirer.prompt([
+                {   type: "input",
+                    name: "n",
+                    message: "Please add the name of the intern."
+                },
+                {   type: "input",
+                    name: "e",
+                    message: "Please add the email of the intern."
+                },
+                {   type: "input",
+                    name: "s",
+                    message: "Please add the school of the intern."
+                }]
+            )
+            const intern = new Intern(internInfo.n, idValue++, internInfo.e, internInfo.s)
+            teamInfo.push(intern)
+        }
+    }
+
+    // After the user has input all employees desired, call the `render` function (required
+    // above) and pass in an array containing all employee objects; the `render` function will
+    // generate and return a block of HTML including templated divs for each employee!
+
+    if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR)
+    fs.writeFileSync(outputPath, render(teamInfo), "utf-8");
+
+    // After you have your html, you're now ready to create an HTML file using the HTML
+    // returned from the `render` function. Now write it to a file named `team.html` in the
+    // `output` folder. You can use the variable `outputPath` above target this location.
+    // Hint: you may need to check if the `output` folder exists and create it if it
+    // does not.
+
+    console.log(`Your Team is Complete!: ${outputPath}`)
+}
+teamData()
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# TeamProfileGenerator
+[**Team Profile Generator**](https://github.com/ryanbrowne360/TeamProfileGenerator.git)
+
+Description:
+
+This application is designed to be used as a command line interface. You can create a "team" via a series of prompts. The program will gather some crucial information about your team members and create an ID for them.
+
+Installation:
+
+The user needs to have Node.js installed, as well as the dependencies included in this program folder. They also need to have a browser which supports javascript. I recomend [Google Chrome](https://www.google.ca/chrome/). First, from the develop folder in your command line type "npm test". This will ensure that the js files in the test folder are able to execute effectively. Once this is successful, make sure you are in the main folder directory of this application in your terminal, and execute the command "node app.js". At this point you should be able to respond to the prompts shown in your CLI.
+
+[Link to Application](https://github.com/ryanbrowne360/TeamProfileGenerator.git)
+
+Usage:
+
+This application makes it easy for the user to build a "team" via the command line UI. They can choose any number of team members. They can choose one manager, and decide whether the other team members are engineers or interns. Once they have completed answering the prompt questions, the application will finalize their data by rendering it into an html page for easy viewing. 
+
+- *Google Chrome*
+- *JavaScript*
+- *Node.js*
+- *HTML*
+- *CSS*
+- *Ryan Browne Software Developer* [Ryan Browne](https://github.com/ryanbrowne360/)
+
+References:
+
+- [W3 Schools](https://www.w3schools.com/)
+- [npm inquirer](https://www.npmjs.com/package/inquirer)
+- [npm jest](https://www.npmjs.com/package/jest)
+- [Node.js](https://nodejs.org/en/)
+
+
+License:
+
+No License is required. You are free to use and modify this program as desired.
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+![HTML](https://img.shields.io/badge/HTML-100%25-orange)
+![CSS](https://img.shields.io/badge/CSS-100%25-yellowgreen)
+
+![image](./Screenshot1.png)
+![image](./Screenshot2.png)
